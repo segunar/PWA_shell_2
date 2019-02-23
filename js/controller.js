10501d63110176b3c053c95677e60c11777e742a
segunar <segunar@gmail.com>
2019-02-23 00:09:56 +0000

    created top menu

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,5 @@
 console.log('cnt')
+
+$("#productBtn").click(function () {
+    topMenuTogle();
+});
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,24 @@
 console.log('fnc')
+
+
+//======================================================================
+//=== SHOW / HIDE TOP NAV MENU ===
+//======================================================================
+function topMenuTogle() {
+    if ($("#productListContainer").hasClass("slideOut")) {
+        showSideNavMenu();
+    }
+    else {
+        hideSideNavMenu();
+    }
+}
+//----------------------------------
+function showSideNavMenu() {
+    $("#productListContainer").removeClass("slideOut");
+    $("#productListContainer").addClass("slideIn");
+}
+//----------------------------------
+function hideSideNavMenu() {
+    $("#productListContainer").removeClass("slideIn");
+    $("#productListContainer").addClass("slideOut");
+}
